2f0433ad64fa6007c92c10cb32d20c96d643189d
Saim <saim@ip-192-168-1-56.eu-central-1.compute.internal>
2025-11-11 16:47:21 +0530

    [SF]:swagger, versioning setup completed

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ErrorHandlerFilter } from './api/global/service/error-handler.filter';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule],
   controllers: [AppController],
   providers: [ErrorHandlerFilter],
 })
